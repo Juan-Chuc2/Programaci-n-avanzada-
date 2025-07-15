b0495c72ad270c7e8fe649198ce6bbd3a18a40ef
Juan Diego <chucjuan508@gmail.com>
2025-07-15 09:24:52 -0800

    3 Commit

--- Actividad 01.py
+++ Actividad 01.py
@@ -6,3 +6,8 @@ print("Hola primer GIT")
 print("Hola primer GIT")
 print("Hola primer GIT")
 print("Hola primer DEV")
+edad=int(input("ingrese su edad"))
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print(" eres menor de edad")
